e6f449fc29e75eb574aea3661d210b64efff6a75
BashirSidi <bashirsalisusidi@gmail.com>
2022-11-14 21:02:36 +0100

    add custom navigation header

--- src/Navigations.js
+++ src/Navigations.js
@@ -1,10 +1,32 @@
 /* eslint-disable no-unused-vars */
 import {NavigationContainer} from "@react-navigation/native";
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from "./DrawerScreens/HomeScreen";
+import CustomNavigationBar from './Components/CustomNavigationBar';
 import CustomNavigationDrawer from "./Components/CustomNavigationDrawer";
 import Profile from './DrawerScreens/ProfileScreen';
+import {Text} from "react-native";
 
+
+const TestStack = createNativeStackNavigator();
+const Test = () => (
+  <TestStack.Navigator
+    screenOptions={{header: props => <CustomNavigationBar {...props} />}}
+  >
+    <TestStack.Screen
+      name="TestStack"
+      component={Bashir}
+      options={{title: 'TestStack.....'}}
+    />
+  </TestStack.Navigator>
+);
+
+const Bashir = () => {
+  return (
+    <Text>Bashir bashir</Text>
+  )
+}
 
 
 const Drawer = createDrawerNavigator();
@@ -14,13 +36,13 @@ const NavigationContents = ({navigation}) => {
     // screenOptions={{drawerPosition:'right'}}
     <Drawer.Navigator
       screenOptions={{
-        headerShown: true
+        headerShown: false
       }}
       drawerContent={props => <CustomNavigationDrawer {...props} /> }
     >
       <Drawer.Screen
         name='Home'
-        component={HomeScreen}
+        component={Test}
       />
       <Drawer.Screen
         name="Profile"
